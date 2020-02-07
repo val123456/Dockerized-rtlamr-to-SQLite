837fa4fb5b9972ea32fa6f701d1a66c2b43d2932
Val <val@val.email>
2020-02-06 19:08:42 -0500

    Add comments ro rtlamr/script.sh.

--- rtlamr/script.sh
+++ rtlamr/script.sh
@@ -1,3 +1,4 @@
+# exit on any error.  Script below will not produce any recoverable errors.
 set -e 
 
 # colors for output
@@ -5,18 +6,25 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo -e "\n${RED}Waiting for rtl_tcp server $RTL_TCP_SERVER on port $RTL_TCP_SERVER_PORT\n"
+
+# Use netcat ( nc ) to connect to rtl_tcp to see if it is up and accepting connections
 until echo "Waiting.." ; sleep 10; nc -z -v -w5 $RTL_TCP_SERVER $RTL_TCP_SERVER_PORT; do :; done
 
 echo -e "\n${RED}Waiting for Python backend server $PYTHON_SERVER on port $PYTHON_SERVER_PORT\n"
+
+# Use netcat ( nc ) to connect to the python application to see if it is up and accepting connections
 until echo "Waiting..."; sleep 10; nc -z -v -w5 $PYTHON_SERVER $PYTHON_SERVER_PORT; do :; done
-# until ping -c 4 -W 2 $PYTHON_SERVER; do :; done
-# wait for server to reopen socket after test connection
-sleep 3
+
+# wait for python app to close and reopen socket after test connection attempt
+sleep 4
 
 echo -e "\n${RED}Looking for meter IDs $METER_IDS\n"
 echo -e "${RED}Looking for meter types $TYPES\n"
+
+# Launch rtlamr with rtl_tcp IP:port, message types, and meters IDS.  Pipe ( | ) to nc to send output to python app in another container.  Background it to enable monitoring
 /root/go/bin/rtlamr -server=$RTL_TCP_SERVER:$RTL_TCP_SERVER_PORT -msgtype=$TYPES -format=json -unique=false -filterid=$METER_IDS | nc -w 1800 $PYTHON_SERVER $PYTHON_SERVER_PORT &
 
+# simple status monitoring.  Checks to see if rltamr and nc programs are running, exits container if either dies.  Runs every 120 seconds
 while true
 do
 
